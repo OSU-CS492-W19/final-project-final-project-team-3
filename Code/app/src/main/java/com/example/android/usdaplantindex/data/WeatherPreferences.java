fb98d0ed2e8490fde9f5919ba246d0e95023bb52
silverware13 <thomasza@oregonstate.edu>
2019-02-28 15:15:06 -0800

    Detailed view plants will not show empty fields

--- Code/app/src/main/java/com/example/android/usdaplantindex/data/WeatherPreferences.java
+++ Code/app/src/main/java/com/example/android/usdaplantindex/data/WeatherPreferences.java
@@ -2,18 +2,9 @@ package com.example.android.usdaplantindex.data;
 
 public class WeatherPreferences {
     private static final String DEFAULT_FORECAST_LOCATION = "Corvallis,OR,US";
-    private static final String DEFAULT_TEMPERATURE_UNITS = "imperial";
-    private static final String DEFAULT_TEMPERATURE_UNITS_ABBR = "F";
 
     public static String getDefaultPlantLocation() {
         return DEFAULT_FORECAST_LOCATION;
     }
-
-    public static String getDefaultTemperatureUnits() {
-        return DEFAULT_TEMPERATURE_UNITS;
-    }
-
-    public static String getDefaultTemperatureUnitsAbbr() {
-        return DEFAULT_TEMPERATURE_UNITS_ABBR;
-    }
 }
+
