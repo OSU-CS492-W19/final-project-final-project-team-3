a3742a1796b5f9dd12017cadea0a4bd5113b4e2e
hemphilc <hemphilc@oregonstate.edu>
2019-03-14 17:25:10 -0700

    Fixed compiler warning -- exportSchema = false

--- Code/app/src/main/java/com/example/android/usdaplantindex/data/PlantDatabase.java
+++ Code/app/src/main/java/com/example/android/usdaplantindex/data/PlantDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
-@Database(entities = {PlantInfo.class}, version = 1)
+@Database(entities = {PlantInfo.class}, version = 1, exportSchema = false)
 public abstract class PlantDatabase extends RoomDatabase {
     private static volatile PlantDatabase INSTANCE;
 
