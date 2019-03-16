17e119bdf7f6102f313faecc3fddddaf0f2e4d7d
silverware13 <thomasza@oregonstate.edu>
2019-03-16 12:15:19 -0700

    Added known issues to About

--- Code/app/src/main/java/com/example/android/usdaplantindex/AboutActivity.java
+++ Code/app/src/main/java/com/example/android/usdaplantindex/AboutActivity.java
@@ -2,18 +2,12 @@ package com.example.android.usdaplantindex;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-
-    private TextView mHeadCitationTV;
-    private TextView mCitationTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        mHeadCitationTV = findViewById(R.id.tv_citation_header);
-        mCitationTV = findViewById(R.id.tv_citation);
     }
 }
